0271768af61447eb89b43c946cbc7c7e2afc8f78
unknown <nikita.pimenov258@gmail.com>
2026-03-22 22:37:07 +0400

    tailwind build fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,10 @@
-module.exports = {
-  content: ["./*.html", "./js/**/*.js"],
-  theme: { extend: {} },
+export default {
+  content: [
+    "./public/**/*.html",  // scan all HTML in public/
+    "./public/js/**/*.js", // scan any JS in public/js/
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
